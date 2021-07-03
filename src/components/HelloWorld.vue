4dc3096c5f16025261046959c3db8c181592755f
a-drobot <a.drobot@elsevier.com>
2021-07-03 20:02:08 +0300

    Part 1 Setting up the environment

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="message">Hello, {{ name }}!</p>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: () => ({
+    name: "World",
+  }),
+}
+</script>
+
+<style scoped>
+.message {
+  font-size: 25px;
+  color: #212121;
+}
+</style>
